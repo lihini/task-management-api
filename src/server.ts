4e7d7f8bc7725fde059ef40feec4ef47e1a15834
Lihini Senanayake <lihini.sena@gmail.com>
2025-04-12 14:13:06 +0530

    chore: configure api documentation

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import tasksRouter from './routes/tasks.route';
 import { errorHandler } from './middlewares/error.middleware';
 import cors from 'cors';
 import morgan from 'morgan';
+import { serve, setup } from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 
@@ -11,6 +13,7 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use('/tasks', tasksRouter);
+app.use('/api-docs', serve, setup(swaggerDocument));
 
 app.get<{}, { message: string }>('/', (req, res) => {
   res.json({
